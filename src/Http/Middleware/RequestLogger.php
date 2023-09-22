1256b2d79a978970ec8866d2c9f6742ed48eab53
Ata <quetzalskeith@gmail.com>
2023-09-22 08:51:47 +0700

    Added try catch to middleware

--- src/Http/Middleware/RequestLogger.php
+++ src/Http/Middleware/RequestLogger.php
@@ -4,8 +4,10 @@ namespace QuetzalStudio\RequestLogger\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use QuetzalStudio\RequestLogger\RequestLogger as Logger;
 use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 class RequestLogger
 {
@@ -18,7 +20,11 @@ class RequestLogger
     {
         $response = $next($request);
 
-        (new Logger)->request($request)->response($response)->create();
+        try {
+            (new Logger)->request($request)->response($response)->create();
+        } catch (Throwable $e) {
+            Log::error($e);
+        }
 
         return $response;
     }
